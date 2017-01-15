2ce5b428edbe8f43cb2ddec79bc8a3f42fd2d6af
Nikita Kukushkin <nk-developer@ya.ru>
2017-01-15 22:11:27 +0300

    Initial commit

--- StackViewHidingWithAnimationBug/ViewController.swift
+++ StackViewHidingWithAnimationBug/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var hideCounter = 0 {
+        didSet {
+            hideCounter = max(0, hideCounter)
+            hideCounterLabel.text = "Hide counter: \(hideCounter)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var animated: Bool {
+        return animationSwitch.isOn
     }
-
-
+    
+    @IBOutlet weak var viewToHide: UIView!
+    @IBOutlet weak var hideCounterLabel: UILabel!
+    @IBOutlet weak var animationSwitch: UISwitch!
+    
+    @IBAction func hide(_ sender: UIButton) {
+        hideCounter += 1
+        if animated {
+            UIView.animate(withDuration: 0.5) {
+                self.viewToHide.isHidden = true
+            }
+        } else {
+            viewToHide.isHidden = true
+        }
+        
+        print("- Hiding, hideCounter: \(hideCounter), hidden: \(viewToHide.isHidden)")
+    }
+    
+    @IBAction func show(_ sender: UIButton) {
+        if animated {
+            UIView.animate(withDuration: 0.5) {
+                self.viewToHide.isHidden = false
+            }
+        } else {
+            viewToHide.isHidden = false
+        }
+        
+        if viewToHide.isHidden {
+            hideCounter -= 1
+        } else {
+            hideCounter = 0
+        }
+        
+        print("+ Showing, hideCounter: \(hideCounter), hidden: \(viewToHide.isHidden)")
+    }
 }
 
